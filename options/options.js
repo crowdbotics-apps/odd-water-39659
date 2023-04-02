09f4de90822a10775487330dc28e09f9ed2d4d94
Crowdbotics <team@crowdbotics.com>
2023-04-02 03:57:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "odd-water-39659",
+  url: "https://odd-water-39659.botics.co",
+  api: "https://odd-water-39659.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
